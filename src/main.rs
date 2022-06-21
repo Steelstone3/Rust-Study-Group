760c4ce9531a5de28ca2e5f21de53351c86fd909
MiyamotoAkira <yefo.akira@twoormore.eu>
2022-06-21 18:44:11 +0100

    Initial setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+
+struct Grid {
+    cells: Vec<bool>,
+    size: usize
+}
+
+impl Grid {
+    fn new(size_of_grid: usize) -> Grid {
+        Grid {
+            cells : vec![],
+            size: size_of_grid
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
