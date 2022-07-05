9bbf816d46ca0ae293ef5cebb0787e2dcc89f324
Jocelyn Facchini <jocelyn.facchini@codurance.com>
2022-07-05 19:22:10 +0100

    Finish implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::CellStatus::{ALIVE, DEAD};
+
 fn main() {
     println!("Hello, world!");
 }
@@ -11,12 +13,7 @@ pub enum CellStatus {
 pub struct Grid {
     height: u8,
     width: u8,
-    cells: Vec<CellStatus>,
-}
-
-pub struct Position {
-    x: u8,
-    y: u8
+    cells: Vec<Vec<CellStatus>>,
 }
 
 // x = 1
@@ -26,28 +23,33 @@ pub struct Position {
 // D D D
 // D A D
 impl Grid {
-    fn with_alives(alive_cell_positions: Vec<(u8, u8)>, height: u8, width: u8) -> Self {
-        let mut cells = vec![CellStatus::DEAD; (height * width) as usize];
-
-        for (x, y) in alive_cell_positions {
-            cells[(y * width + x) as usize] = CellStatus::ALIVE
-        }
-
-        Grid {
-            height,
-            width,
-            cells
-        }
-    }
-
     fn count_neighbours_at(&self, x: u8, y: u8) -> u8 {
         let mut neighbours = 0;
+
+        if self.is_alive_at(x + 1, y) {
+            neighbours += 1;
+        }
+        if self.is_alive_at(x, y + 1) {
+            neighbours += 1;
+        }
+        if self.is_alive_at(x + 1, y + 1) {
+            neighbours += 1;
+        }
+
+        if x > 0 && self.is_alive_at(x - 1, y + 1) {
+            neighbours += 1;
+        }
+        if y > 0 && self.is_alive_at(x + 1, y - 1) {
+            neighbours += 1;
+        }
 
         if x > 0 && self.is_alive_at(x - 1, y) {
             neighbours += 1;
         }
-
-        if self.is_alive_at(x + 1, y) {
+        if y > 0 && self.is_alive_at(x, y - 1) {
+            neighbours += 1;
+        }
+        if x > 0 && y > 0 && self.is_alive_at(x - 1, y - 1) {
             neighbours += 1;
         }
 
@@ -56,61 +58,148 @@ impl Grid {
 
     fn is_alive_at(&self, x: u8, y: u8) -> bool {
         match self.cell_at(x, y) {
-            Some(CellStatus::ALIVE) => true,
+            Some(ALIVE) => true,
             _ => false,
         }
     }
 
     fn cell_at(&self, x: u8, y: u8) -> Option<&CellStatus> {
-        self.cells.get((y * self.width + x) as usize)
+        let line = self.cells.get(x as usize);
+        match line {
+            Some(cells) => cells.get(y as usize),
+            _ => None
+        }
     }
 }
 
 pub fn next_generation(previous_generation: Grid) -> Grid {
-    let mut next_generation: Vec<(u8, u8)> = vec![];
+    let mut next_generation: Vec<Vec<CellStatus>> = Vec::new();
 
-    for y in 0..previous_generation.height {
-        for x in 0..previous_generation.width {
+    for x in 0..previous_generation.height {
+        let mut line: Vec<CellStatus> = Vec::new();
+        for y in 0..previous_generation.width {
             let neighbours = previous_generation.count_neighbours_at(x, y);
 
-            if neighbours == 2 {
-                if previous_generation.is_alive_at(x, y) {
-                    next_generation.push((x, y))
-                }
+            match (neighbours, previous_generation.cell_at(x, y)) {
+                (2 | 3, Some(&ALIVE)) => {
+                    line.push(ALIVE)
+                },
+                (3, Some(&DEAD)) => {
+                    line.push(ALIVE)
+                },
+                _ => line.push(DEAD)
             }
         }
+        next_generation.push(line);
     }
 
-    Grid::with_alives(next_generation, previous_generation.width, previous_generation.height)
+    Grid {
+        cells: next_generation,
+        width: previous_generation.width,
+        height: previous_generation.height
+    }
 }
 
 #[cfg(test)]
 mod game_of_life_should {
     use super::*;
-    use CellStatus::{ALIVE,DEAD};
+    use CellStatus::{ALIVE, DEAD};
 
     #[test]
     fn let_cell_die_because_it_has_no_friends() {
-        let previous_generation = Grid::with_alives(vec![(0, 0)], 3, 3);
+        let previous_generation = Grid {
+            cells: vec![
+                vec![ALIVE, DEAD, DEAD],
+                vec![DEAD, DEAD, DEAD],
+                vec![DEAD, DEAD, DEAD],
+            ],
+            height: 3,
+            width: 3,
+        };
 
         assert_eq!(next_generation(previous_generation).cells, vec![
-            DEAD, DEAD, DEAD,
-            DEAD, DEAD, DEAD,
-            DEAD, DEAD, DEAD
+            vec![DEAD, DEAD, DEAD],
+            vec![DEAD, DEAD, DEAD],
+            vec![DEAD, DEAD, DEAD],
         ]);
     }
 
     #[test]
     fn let_cell_live_because_the_cell_has_2_friendly_neighbourhood_cell() {
-        let previous_generation = Grid::with_alives(vec![(0, 0), (1, 0), (2, 0)], 3, 3);
+        let previous_generation = Grid {
+            cells: vec![
+                vec![ALIVE, ALIVE, ALIVE],
+                vec![DEAD, DEAD, DEAD],
+                vec![DEAD, DEAD, DEAD],
+            ],
+            height: 3,
+            width: 3,
+        };
 
         assert_eq!(
             next_generation(previous_generation).cells,
             vec![
-                DEAD, ALIVE, DEAD,
-                DEAD, DEAD,  DEAD,
-                DEAD, DEAD,  DEAD
+                vec![DEAD, ALIVE, DEAD],
+                vec![DEAD, ALIVE, DEAD],
+                vec![DEAD, DEAD, DEAD],
             ]
         );
+    }
+
+    #[test]
+    fn let_cell_die_because_it_has_no_friends_plus_some_alive_cells() {
+        let previous_generation = Grid {
+            cells: vec![
+                vec![ALIVE, DEAD, DEAD],
+                vec![DEAD, DEAD, ALIVE],
+                vec![DEAD, ALIVE, ALIVE],
+            ],
+            height: 3,
+            width: 3,
+        };
+
+        assert_eq!(next_generation(previous_generation).cells, vec![
+            vec![DEAD, DEAD, DEAD],
+            vec![DEAD, DEAD, ALIVE],
+            vec![DEAD, ALIVE, ALIVE],
+        ]);
+    }
+
+    #[test]
+    fn dead_cell_with_three_neighbours_is_alive() {
+        let previous_generation = Grid {
+            cells: vec![
+                vec![DEAD, DEAD, DEAD],
+                vec![DEAD, DEAD, ALIVE],
+                vec![DEAD, ALIVE, ALIVE],
+            ],
+            height: 3,
+            width: 3,
+        };
+
+        assert_eq!(next_generation(previous_generation).cells, vec![
+            vec![DEAD, DEAD, DEAD],
+            vec![DEAD, ALIVE, ALIVE],
+            vec![DEAD, ALIVE, ALIVE],
+        ]);
+    }
+
+    #[test]
+    fn alive_cell_with_three_neighbours_stays_alive() {
+        let previous_generation = Grid {
+            cells: vec![
+                vec![DEAD, DEAD, DEAD],
+                vec![DEAD, ALIVE, ALIVE],
+                vec![DEAD, ALIVE, ALIVE],
+            ],
+            height: 3,
+            width: 3,
+        };
+
+        assert_eq!(next_generation(previous_generation).cells, vec![
+            vec![DEAD, DEAD, DEAD],
+            vec![DEAD, ALIVE, ALIVE],
+            vec![DEAD, ALIVE, ALIVE],
+        ]);
     }
 }
