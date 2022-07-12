b183e6ce7270efa87404677c0f2a38de756b7d4a
Mattsi <mattsi.jansky@codurance.com>
2022-07-12 18:50:53 +0100

    Increase size of bench

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,19 +1,32 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use conways_game_of_life::{Grid, next_generation};
+use rand::Rng;
+use conways_game_of_life::{CellStatus, Grid, next_generation};
 use conways_game_of_life::CellStatus::{ALIVE, DEAD};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let grid = Grid {
-        cells: vec![
-            vec![DEAD, DEAD, DEAD],
-            vec![DEAD, ALIVE, ALIVE],
-            vec![DEAD, ALIVE, ALIVE],
-        ],
+        cells: generate_test_data(1000),
         height: 3,
         width: 3,
     };
 
     c.bench_function("next generation", |b| b.iter(|| next_generation(grid.clone())));
+}
+
+fn generate_test_data(size: usize) -> Vec<Vec<CellStatus>> {
+    let mut result = vec![];
+    let mut rng = rand::thread_rng();
+
+    for i in 0..=size {
+        let mut inner = vec![];
+        for i in 0..=size {
+            let rand_value = rand::thread_rng().gen();
+            inner.push(if rand_value { DEAD } else { ALIVE })
+        }
+        result.push(inner);
+    }
+
+    result
 }
 
 criterion_group!(benches, criterion_benchmark);
