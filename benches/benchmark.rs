8a96fee39e7d1e5b91d43431184428c0e6c6cce5
Mattsi <mattsi.jansky@codurance.com>
2022-07-14 20:42:48 +0100

    Basic multi-threading implementation

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -2,7 +2,6 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use rand::prelude::StdRng;
 use rand::{Rng, SeedableRng};
 use conways_game_of_life::{CellStatus, Grid, next_generation};
-use conways_game_of_life::CellStatus::{ALIVE, DEAD};
 
 pub fn criterion_benchmark(c: &mut Criterion) {
     let grid = Grid {
@@ -22,7 +21,7 @@ fn generate_test_data(size: usize) -> Vec<Vec<CellStatus>> {
         let mut inner = vec![];
         for i in 0..=size {
             let rand_value = rng.gen();
-            inner.push(if rand_value { DEAD } else { ALIVE })
+            inner.push(if rand_value { false } else { true })
         }
         result.push(inner);
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
-use crate::CellStatus::{ALIVE, DEAD};
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use std::sync::mpsc::sync_channel;
+use std::thread;
 
-#[derive(PartialEq, Debug, Clone)]
-pub enum CellStatus {
-    ALIVE,
-    DEAD,
-}
+pub type CellStatus = bool;
 
 #[derive(Clone)]
 pub struct Grid {
@@ -13,12 +12,6 @@ pub struct Grid {
     pub cells: Vec<Vec<CellStatus>>,
 }
 
-// x = 1
-// y = 2
-// pos = 7 = y * width + x
-// D D D
-// D D D
-// D A D
 impl Grid {
     fn count_neighbours_at(&self, x: u8, y: u8) -> u8 {
         let mut neighbours = 0;
@@ -55,7 +48,7 @@ impl Grid {
 
     fn is_alive_at(&self, x: u8, y: u8) -> bool {
         match self.cell_at(x, y) {
-            Some(ALIVE) => true,
+            Some(true) => true,
             _ => false,
         }
     }
@@ -71,53 +64,74 @@ impl Grid {
 
 pub fn next_generation(previous_generation: Grid) -> Grid {
     let mut next_generation: Vec<Vec<CellStatus>> = Vec::new();
+    let height = previous_generation.height;
+    let width = previous_generation.width;
+    let previous_generation = Arc::new(RwLock::new(previous_generation));
+    let (tx, rx) = sync_channel(height as usize);
 
-    for x in 0..previous_generation.height {
-        let mut line: Vec<CellStatus> = Vec::new();
-        for y in 0..previous_generation.width {
-            let neighbours = previous_generation.count_neighbours_at(x, y);
+    let mut handles = vec![];
 
-            match (neighbours, previous_generation.cell_at(x, y)) {
-                (2 | 3, Some(&ALIVE)) => {
-                    line.push(ALIVE)
-                },
-                (3, Some(&DEAD)) => {
-                    line.push(ALIVE)
-                },
-                _ => line.push(DEAD)
+    for x in 0..height {
+        let shared_previous_generation = previous_generation.clone();
+        let tx = tx.clone();
+        handles.push(thread::spawn(move || {
+            let shared_previous_generation = shared_previous_generation.read().expect("Unable to get read-access to previous generation");
+            let mut line: Vec<CellStatus> = Vec::new();
+                for y in 0..shared_previous_generation.width {
+                    let neighbours = shared_previous_generation.count_neighbours_at(x, y);
+
+                    match (neighbours, shared_previous_generation.cell_at(x, y)) {
+                        (2 | 3, Some(&true)) => {
+                            line.push(true)
+                        },
+                        (3, Some(&false)) => {
+                            line.push(true)
+                        },
+                        _ => line.push(false)
+                    }
+                }
+            tx.send((x, line))
             }
-        }
-        next_generation.push(line);
+        ));
+    }
+    drop(tx);
+
+    let mut messages = HashMap::new();
+    while let Ok((x, line)) = rx.recv() {
+        messages.insert(x,line);
+    }
+
+    for i in 0..height {
+        next_generation.push(messages.remove(&i).unwrap());
     }
 
     Grid {
         cells: next_generation,
-        width: previous_generation.width,
-        height: previous_generation.height
+        width,
+        height
     }
 }
 
 #[cfg(test)]
 mod game_of_life_should {
     use super::*;
-    use CellStatus::{ALIVE, DEAD};
 
     #[test]
     fn let_cell_die_because_it_has_no_friends() {
         let previous_generation = Grid {
             cells: vec![
-                vec![ALIVE, DEAD, DEAD],
-                vec![DEAD, DEAD, DEAD],
-                vec![DEAD, DEAD, DEAD],
+                vec![true, false, false],
+                vec![false, false, false],
+                vec![false, false, false],
             ],
             height: 3,
             width: 3,
         };
 
         assert_eq!(next_generation(previous_generation).cells, vec![
-            vec![DEAD, DEAD, DEAD],
-            vec![DEAD, DEAD, DEAD],
-            vec![DEAD, DEAD, DEAD],
+            vec![false, false, false],
+            vec![false, false, false],
+            vec![false, false, false],
         ]);
     }
 
@@ -125,9 +139,9 @@ mod game_of_life_should {
     fn let_cell_live_because_the_cell_has_2_friendly_neighbourhood_cell() {
         let previous_generation = Grid {
             cells: vec![
-                vec![ALIVE, ALIVE, ALIVE],
-                vec![DEAD, DEAD, DEAD],
-                vec![DEAD, DEAD, DEAD],
+                vec![true, true, true],
+                vec![false, false, false],
+                vec![false, false, false],
             ],
             height: 3,
             width: 3,
@@ -136,9 +150,9 @@ mod game_of_life_should {
         assert_eq!(
             next_generation(previous_generation).cells,
             vec![
-                vec![DEAD, ALIVE, DEAD],
-                vec![DEAD, ALIVE, DEAD],
-                vec![DEAD, DEAD, DEAD],
+                vec![false, true, false],
+                vec![false, true, false],
+                vec![false, false, false],
             ]
         );
     }
@@ -147,18 +161,18 @@ mod game_of_life_should {
     fn let_cell_die_because_it_has_no_friends_plus_some_alive_cells() {
         let previous_generation = Grid {
             cells: vec![
-                vec![ALIVE, DEAD, DEAD],
-                vec![DEAD, DEAD, ALIVE],
-                vec![DEAD, ALIVE, ALIVE],
+                vec![true, false, false],
+                vec![false, false, true],
+                vec![false, true, true],
             ],
             height: 3,
             width: 3,
         };
 
         assert_eq!(next_generation(previous_generation).cells, vec![
-            vec![DEAD, DEAD, DEAD],
-            vec![DEAD, DEAD, ALIVE],
-            vec![DEAD, ALIVE, ALIVE],
+            vec![false, false, false],
+            vec![false, false, true],
+            vec![false, true, true],
         ]);
     }
 
@@ -166,18 +180,18 @@ mod game_of_life_should {
     fn dead_cell_with_three_neighbours_is_alive() {
         let previous_generation = Grid {
             cells: vec![
-                vec![DEAD, DEAD, DEAD],
-                vec![DEAD, DEAD, ALIVE],
-                vec![DEAD, ALIVE, ALIVE],
+                vec![false, false, false],
+                vec![false, false, true],
+                vec![false, true, true],
             ],
             height: 3,
             width: 3,
         };
 
         assert_eq!(next_generation(previous_generation).cells, vec![
-            vec![DEAD, DEAD, DEAD],
-            vec![DEAD, ALIVE, ALIVE],
-            vec![DEAD, ALIVE, ALIVE],
+            vec![false, false, false],
+            vec![false, true, true],
+            vec![false, true, true],
         ]);
     }
 
@@ -185,18 +199,18 @@ mod game_of_life_should {
     fn alive_cell_with_three_neighbours_stays_alive() {
         let previous_generation = Grid {
             cells: vec![
-                vec![DEAD, DEAD, DEAD],
-                vec![DEAD, ALIVE, ALIVE],
-                vec![DEAD, ALIVE, ALIVE],
+                vec![false, false, false],
+                vec![false, true, true],
+                vec![false, true, true],
             ],
             height: 3,
             width: 3,
         };
 
         assert_eq!(next_generation(previous_generation).cells, vec![
-            vec![DEAD, DEAD, DEAD],
-            vec![DEAD, ALIVE, ALIVE],
-            vec![DEAD, ALIVE, ALIVE],
+            vec![false, false, false],
+            vec![false, true, true],
+            vec![false, true, true],
         ]);
     }
 }
