bff27b736b3af1d05aac1ec2500846440c0100a8
Mattsi <mattsi.jansky@codurance.com>
2022-08-09 18:33:55 +0100

    Refactor for simplicity / readability

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,19 @@ pub struct BowlingGame {
     rolls: u16,
     score: u16,
     max_rolls: u16,
-    active_frame: Option<Frame>,
+    active_frame: Frame,
+    last_frame_was_spare: bool
 }
 
 pub struct Frame {
-    roll_one_pins: u16,
+    roll_one_pins: Option<u16>,
     roll_two_pins: Option<u16>,
 }
 
 impl Frame {
     pub fn is_spare(&self) -> bool {
         if let Some(roll_two_pins) = self.roll_two_pins {
-            self.roll_one_pins + roll_two_pins == 10
+            self.roll_one_pins.unwrap() + roll_two_pins == 10
         } else {
             false
         }
@@ -36,15 +37,26 @@ impl BowlingGame {
             rolls: 0,
             max_rolls: 20,
             score: 0,
-            active_frame: None,
+            active_frame: Frame {
+                roll_one_pins: None,
+                roll_two_pins: None,
+            },
+            last_frame_was_spare: false
         }
     }
 
-    pub fn reset_active_frame(&mut self, pins: u16) {
-        self.active_frame = Some(Frame {
-            roll_one_pins: pins,
+    pub fn reset_active_frame(&mut self) {
+        self.active_frame = Frame {
+            roll_one_pins: None,
             roll_two_pins: None,
-        });
+        };
+    }
+
+    pub fn reset_active_frame_with_first_roll(&mut self, pins: u16) {
+        self.active_frame = Frame {
+            roll_one_pins: Some(pins),
+            roll_two_pins: None,
+        };
     }
 
     pub fn roll(&mut self, pins: u16) -> Result<(), Error> {
@@ -59,22 +71,25 @@ impl BowlingGame {
         self.rolls += 1;
         self.score += pins;
 
-        if let Some(ref mut frame) = self.active_frame {
-            if frame.is_spare() {
+        if self.active_frame.roll_one_pins.is_none() {
+            self.active_frame.roll_one_pins = Some(pins);
+
+            if self.last_frame_was_spare {
                 if !(self.is_game_complete()) {
                     self.score += pins;
                 }
-                self.reset_active_frame(pins);
-            } else if frame.is_second_roll_in_frame() {
-                frame.roll_two_pins = Some(pins);
-                if self.is_spare_last_frame() {
+                self.last_frame_was_spare = false;
+            }
+        }
+        else if self.active_frame.is_second_roll_in_frame() {
+            self.active_frame.roll_two_pins = Some(pins);
+            if self.active_frame.is_spare() {
+                self.last_frame_was_spare = true;
+                if self.is_last_frame() {
                     self.max_rolls += 1;
                 }
-            } else {
-                self.reset_active_frame(pins);
             }
-        } else {
-            self.reset_active_frame(pins);
+            self.reset_active_frame()
         }
 
         Ok(())
@@ -88,8 +103,8 @@ impl BowlingGame {
         Some(self.score)
     }
 
-    fn is_spare_last_frame(&self) -> bool {
-        self.active_frame.as_ref().unwrap().is_spare() && self.rolls == self.max_rolls
+    fn is_last_frame(&self) -> bool {
+        self.rolls == self.max_rolls
     }
 
     fn is_game_complete(&self) -> bool {
