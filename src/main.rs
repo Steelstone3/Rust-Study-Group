ff4fb958c5c968931fd118e54e197cfc12fe6324
MiyamotoAkira <yefo.akira@twoormore.eu>
2022-08-30 18:43:53 +0100

    Initial basic test for the bank kata

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,49 @@ fn main() {
     println!("Hello, world!");
 }
 
+trait AccountService {
+    fn deposit(&mut self, amount: u32);
+    fn withdraw(&mut self, amount: u32);
+    fn print(&self) -> String;
+}
+
+trait Repository {
+    fn add(&self, account: Account);
+    fn retrieve(&self);
+}
+
+struct Account {}
+
+impl Account {
+    fn new() -> Account {
+        Account {}
+    }
+}
+
+impl AccountService for Account {
+    fn deposit(&mut self, amount: u32) {
+        todo!()
+    }
+
+    fn withdraw(&mut self, amount: u32) {
+        todo!()
+    }
+
+    fn print(&self) -> String {
+        "Date || Amount || Balance\n".to_string()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn call_print_function_no_actions() {
+        let account = Account::new();
+
+        let result = account.print();
+
+        assert_eq!(result, "Date || Amount || Balance\n");
+    }
 }
